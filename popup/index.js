a08a1ce2e6990a45c2e08eb33982c71d6e359764
Uğur kellecioğlu <ugur.kellecioglu@outlook.com>
2023-05-29 18:16:46 +0300

    feat: update ui as switch changes

--- popup/index.js
+++ popup/index.js
@@ -1,16 +1,33 @@
 var statusSwitch = document.getElementById("status-switchbox")
-
+var statusText = document.querySelector(".status")
+var paragraphText = document.querySelector(".container.status-wrapper p")
+var takeBreakBtn = document.querySelector(".take-break-btn")
 // Load initial switch state from chrome.storage.local
 chrome.storage.local.get("switchState", function (result) {
   if (result.switchState !== undefined) {
     statusSwitch.checked = result.switchState
+    updateStatusText(result.switchState)
   }
 })
 
 // Add event listener to the switch
 statusSwitch.addEventListener("change", function () {
   var switchState = statusSwitch.checked
+  updateStatusText(switchState)
 
   // Save the switch state in chrome.storage.local
   chrome.storage.local.set({ switchState: switchState })
 })
+
+// Function to update the status text based on the switch state
+function updateStatusText(state) {
+  if (state) {
+    statusText.textContent = "Focus mode is on"
+    paragraphText.textContent = "Distracting websites are now blocked"
+    takeBreakBtn.style.display = "block"
+  } else {
+    statusText.textContent = "Focus mode is off"
+    paragraphText.textContent = "Turn on to block distracting websites"
+    takeBreakBtn.style.display = "none"
+  }
+}
