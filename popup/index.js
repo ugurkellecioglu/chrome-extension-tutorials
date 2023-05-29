e8bdbc852c854143f1f48f7f8f1fe050bd39b0dd
Uğur kellecioğlu <ugur.kellecioglu@outlook.com>
2023-05-29 18:21:00 +0300

    feat: store entered websites as an array in storage and render them

--- popup/index.js
+++ popup/index.js
@@ -31,3 +31,62 @@ function updateStatusText(state) {
     takeBreakBtn.style.display = "none"
   }
 }
+
+// input & save websites on storage
+
+var inputField = document.querySelector(".form-control")
+var listOfWebsites = document.querySelector(".list-of-websites")
+
+// Load initial website list from chrome.storage.local
+chrome.storage.local.get("websites", function (result) {
+  if (result.websites !== undefined) {
+    displayWebsites(result.websites)
+  }
+})
+
+// Function to display the websites in the list
+function displayWebsites(websites) {
+  listOfWebsites.innerHTML = ""
+  for (var i = 0; i < websites.length; i++) {
+    var website = websites[i]
+    var websiteItem = document.createElement("div")
+    websiteItem.textContent = website
+    listOfWebsites.appendChild(websiteItem)
+  }
+}
+
+// Event listener for the input field
+inputField.addEventListener("keydown", function (event) {
+  if (event.key === "Enter") {
+    if (!isValidURL(inputField.value)) {
+      alert("Please enter a valid URL")
+      return
+    }
+
+    var inputValue = inputField.value.trim()
+    // Retrieve the current list of websites from chrome.storage.local
+    chrome.storage.local.get("websites", function (result) {
+      var websites = result.websites || []
+      websites.push(inputValue)
+
+      // Save the updated list of websites in chrome.storage.local
+      chrome.storage.local.set({ websites: websites }, function () {
+        // Display the updated list of websites
+        displayWebsites(websites)
+
+        // Clear the input field
+        inputField.value = ""
+      })
+    })
+  }
+})
+
+function isValidURL(string) {
+  try {
+    new URL(string)
+  } catch (_) {
+    return false
+  }
+
+  return true
+}
